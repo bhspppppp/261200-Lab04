d3d8ea94ecdd55b31cd9548d953302729a5dc58f
bhspppppp <beach.p211@gmail.com>
2026-07-20 01:14:53 +0700

    final commit

--- Lab04.java
+++ Lab04.java
@@ -1,16 +1,16 @@
 public class Lab04 {
     public static void main(String[] args) {
-        HealthRecord h1 = new HealthRecord(170);
-        HealthRecord h2 = new HealthRecord(160);
-        HealthRecord h3 = new HealthRecord(180);
-        HealthRecord h4 = new HealthRecord(300);
+        // Create and test student records
+        HealthRecord student1 = new HealthRecord(120);
+        HealthRecord student2 = new HealthRecord(55);
+        HealthRecord student3 = new HealthRecord(280);
 
-        System.out.println(h1.getDetails());
-        System.out.println(h2.getDetails());
-        System.out.println(h3.getDetails());
-        System.out.println(h4.getDetails());
+        // Display individual records
+        System.out.println(student1.getDetails()); // Expected: 120
+        System.out.println(student2.getDetails()); // Expected: 55
+        System.out.println(student3.getDetails()); // Expected: 165 (default, as 280 is invalid)
 
-        System.out.println();
-        System.out.println(HealthRecord.getClassDetails());
+        // Display class statistics
+        System.out.println(HealthRecord.getClassDetails()); // Expected: tallest=120, shortest=55
     }
 }
